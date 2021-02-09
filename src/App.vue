c95be181abea933e4b6e356fbfd82aaccf6851d0
Terzea Bogdan <terzeabogdan2007@yahoo.com>
2021-02-09 10:13:05 +0200

    Customize my project template and learn basics

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <h1>{{ title }}</h1>
 </template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      title: 'My vue app'
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
@@ -17,16 +22,9 @@
   color: #2c3e40;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
